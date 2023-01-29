2cabe52fa29d202bb6af169c596a2b757daad8d6
VladFominov <111357638+VladFominov@users.noreply.github.com>
2023-01-29 10:44:13 +0200

    live

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import resume from "../../assets/images/Vlad_Fominov_junior_frontend.pdf";
+
 function Resume() {
   return (
-    <div>this is my Resume</div>
-  )
+    <div>
+      this is my Resume
+      <a class="btn btn-primary" href={resume} role="button" download="Resume">
+        RESUME
+      </a>
+    </div>
+  );
 }
 
 export default Resume;
